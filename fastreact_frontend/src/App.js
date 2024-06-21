8c427a57b1d9a7862d3f0ef0a3a0162268bd25ff
cleversonkleber <cleversonkleber@gmail.com>
2024-06-20 22:10:03 -0300

    v1

---
+++ fastreact_frontend/src/App.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+
+function App() {
+  const [body, setBody]=useState('');
+  useEffect(()=>{
+    axios
+      .get("http://localhost:7777/get_data")
+      .then((response)=>{
+        const data = response.data
+        setBody(data['body'])
+      })
+      .catch((e)=>{
+        //
+      })
+
+  },[])
+  return (
+    <div>
+      {body}
+    </div>
+  );
+}
+
+export default App;
